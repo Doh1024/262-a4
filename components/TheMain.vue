12b49cfe658bed23b7dc40909678a205773d2b26
Doh1024 <dohyeongkim001024@gmail.com>
2022-11-29 21:21:32 -0600

    tweaks

--- components/TheMain.vue
+++ components/TheMain.vue
@@ -1,3 +1,12 @@
+<script setup>
+  defineProps({
+    main:{
+      type: String,
+      default: "this is main content"
+    }
+  });
+</script>
+
 <template>
-  
+  <div class="flex justify-center text-center my-5 text-xl">{{main}}</div>
 </template>
